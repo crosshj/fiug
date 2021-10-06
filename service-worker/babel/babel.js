4d2d383f5a2e97752ba721c0ddbbd8f0399f574a
crosshj <github@chimpjuice.com>
2021-10-06 20:23:58 +0000

    v0.4.6

--- service-worker/babel/babel.js
+++
@@ -1,4 +0,0 @@
-//import "https://unpkg.com/@babel/standalone/babel.min.js";
-import "https://unpkg.com/@babel/standalone@7.15.7/babel.min.js";
-
-export default Babel;
--- terminal/.example_import2.js
+++
@@ -1,3 +0,0 @@
-console.log('import happened');
-
-export const sleep = (time) => new Promise((resolve)=> setTimeout(() => resolve('done'), time) );
